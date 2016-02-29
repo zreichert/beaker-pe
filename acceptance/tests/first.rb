01c6d389d61ee002bf3e71101be3e5148538468a
kevpl <kevin.imber@puppetlabs.com>
2016-02-29 12:40:31 -0800

    updated acceptance testing for beaker-pe

--- acceptance/tests/first.rb
+++ acceptance/tests/first.rb
@@ -3,6 +3,12 @@
 # Each file corresponding to a new test made up of individual testing steps
 test_name "Template Acceptance Test Example"
 
-step "Fail fast!"
+# At this point, acceptance testing of beaker-pe has been left in beaker itself,
+# since that requires no work at this point since beaker includes this.
+# TODO note that when things change for Beaker 3.0, testing will have to be
+#   changed to match a different pattern, where the `install_pe` acceptance
+#   testing will all be done in this library's testing.
 
-fail_test("There are no acceptance tests yet!")
+# step "Fail fast!"
+
+# fail_test("There are no acceptance tests yet!")
