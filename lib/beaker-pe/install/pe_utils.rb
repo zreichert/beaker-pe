33cdfef03a945e1e982bfb6b198f6aceaa39418f
Brandon High <brandon.high@puppetlabs.com>
2016-07-06 14:02:09 -0700

    (PE-15351) Fix use of -c/-f flags on upgrades
    Prior to this commit the condition used to decide whether to use
    the `-c`/`-f` flags was dependent on `host['pe_upgrade_ver']` and
    `host['pe_ver']` which was an unreliable condition.
    This commit updates the condition to determine whether to use the
    `-f` flag to simply look at `host['pe_ver']` because that value
    is updated depending on what version of pe is currently being
    installed or upgraded to.
    The condition to decide to omit the `-c` flag has to depend on
    `opts[:HOSTS][host.name][:pe_ver]` because that value is not
    modified during upgrade and can be used for a valid comparison
    to determine if the install will have a `pe.conf` file to use
    for an upgrade.

--- lib/beaker-pe/install/pe_utils.rb
+++ lib/beaker-pe/install/pe_utils.rb
@@ -110,14 +110,14 @@ module Beaker
           else
             pe_debug = host[:pe_debug] || opts[:pe_debug]  ? ' -D' : ''
             pe_cmd = "cd #{host['working_dir']}/#{host['dist']} && ./#{host['pe_installer']}#{pe_debug}"
-            if ! version_is_less(host['pe_ver'], '2016.2.1') && !(opts[:type] == :upgrade && version_is_less(host['pe_upgrade_ver'], '2016.2.1') )
+            if ! version_is_less(host['pe_ver'], '2016.2.1')
               # -f option forces non-interactive mode
               pe_cmd += " -f"
             end
 
             # If there are no answer overrides, and we are doing an upgrade from 2016.2.0,
             # we can assume there will be a valid pe.conf in /etc that we can re-use.
-            if opts[:answers].nil? && opts[:custom_answers].nil? && opts[:type] == :upgrade && !version_is_less(host['pe_ver'], '2016.2.0')
+            if opts[:answers].nil? && opts[:custom_answers].nil? && opts[:type] == :upgrade && !version_is_less(opts[:HOSTS][host.name][:pe_ver], '2016.2.0')
               "#{pe_cmd}"
             else
               "#{pe_cmd} #{host['pe_installer_conf_setting']}"
