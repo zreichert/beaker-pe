zreichert/beaker-pe

commit f9cb3ca802ddc94a9a42dcbcfef96f7a76a19dff

(PE-20405) On frictionless installs ensure all hosts run prepare_hosts
Previously when there was a frictionless install prepare_hosts was
only set on the master. This meant that agents were skipped and as
a result agents did not have a working_dir set.
This caused unix machines to install on just the root directory and
caused failures on windows.

lib/beaker-pe/install/pe_utils.rb
@@ -450,8 +450,8 @@ module Beaker
           all_hosts = [master, *agents]
           configure_type_defaults_on(all_hosts)
 
-          # Set PE distribution on the master, create working dir
-          prepare_hosts([master], opts)
+          # Set PE distribution on the agents, creates working directories
+          prepare_hosts(all_hosts, opts)
           fetch_pe([master], opts)
           prepare_host_installer_options(master)
           generate_installer_conf_file_for(master, [master], opts)

spec/beaker-pe/install/pe_utils_spec.rb
@@ -1357,6 +1357,11 @@ describe ClassMixedWithDSLInstallUtils do
 
       subject.simple_monolithic_install(monolithic, agents)
     end
+
+    it "calls prepare_hosts on all hosts instead of just master" do
+      expect(subject).to receive(:prepare_hosts).with([monolithic] + [el_agent, el_agent, el_agent], {})
+      subject.simple_monolithic_install(monolithic, [el_agent, el_agent, el_agent])
+    end
   end
 
   describe 'do_higgs_install' do

commit 19c1e64a93eb39ba09365cace892806c23ce1a75

(maint) Fix parallel agent install Parallel agent installs have been broken since f9cb3ca802ddc94a9a42dcbcfef96f7a76a19dff because the install command contained a working_dir that was unique per host. This fixes that issue and also allows all agents to be installed in parallel regardless of OS or unique installer_cmd

lib/beaker-pe/install/pe_utils.rb
@@ -470,8 +470,8 @@ module Beaker
           end
 
           step "Install agents" do
-            agents.group_by {|agent| installer_cmd(agent, opts)}.each do |cmd, agents|
-              on agents, cmd, :run_in_parallel => true
+            block_on(agents, {:run_in_parallel => true}) do |host|
+              on(host, installer_cmd(host, opts))
             end
           end
 

spec/beaker-pe/install/pe_utils_spec.rb
@@ -1357,14 +1357,13 @@ describe ClassMixedWithDSLInstallUtils do
 
     it 'installs on the master then on the agents' do
       expect(subject).to receive(:on).with(monolithic, "install master").ordered
-      expect(subject).to receive(:on).with([el_agent, el_agent], "install el agent", anything()).ordered
+      expect(subject).to receive(:block_on).with([el_agent, el_agent], :run_in_parallel => true).ordered
 
       subject.simple_monolithic_install(monolithic, [el_agent, el_agent])
     end
 
-    it 'installs agents in parallel if their install command is the same' do
-      expect(subject).to receive(:on).with([el_agent, el_agent], "install el agent", :run_in_parallel => true)
-      expect(subject).to receive(:on).with([deb_agent, deb_agent], "install deb agent", :run_in_parallel => true)
+    it 'installs agents in parallel' do
+      expect(subject).to receive(:block_on).with([el_agent, el_agent, deb_agent, deb_agent], :run_in_parallel => true)
 
       subject.simple_monolithic_install(monolithic, [el_agent, el_agent, deb_agent, deb_agent])
     end